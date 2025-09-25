14a93afb71ce80694d754e19866409c2a59dab3e
coolvp <coolvansh1503@gmail.com>
2025-09-25 11:38:44 +0530

    Add files via upload

---
+++ toh.cpp
@@ -0,0 +1,41 @@
+// Write a program to implement the Tower of Hanoi using recursion.
+#include<iostream.h>
+#include<conio.h>
+class toh {
+    int n;
+    char a, b, c;
+public:
+    toh() {
+    }
+
+    toh(int no, char t1, char t3, char t2) {
+        n = no;
+        a = t1;
+        b = t2;
+        c = t3;
+    }
+
+    void tower(int n, char a, char c, char b) {
+        if (n == 1) {
+            cout << "\n Disk 1 moved from Tower " << a << " to Tower " << c;
+            return;
+        }
+        tower(n - 1, a, b, c);
+        cout << "\n Disk " << n << " moved from Tower " << a << " to Tower " << c;
+        tower(n - 1, b, c, a);
+    }
+
+    void start() {
+	tower(n, a, c, b);
+    }
+};
+
+int main() {
+    int n;
+    clrscr();
+    cout << "\t\t\t TOWER OF HANOI \n\n";
+    cout << "Enter the value of n : ";
+    cin >> n;
+    toh t(n, 'A', 'C', 'B');
+    t.start(); // Call the correct function
+}
